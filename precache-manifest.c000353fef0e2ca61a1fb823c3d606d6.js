3b24b7e43d73e495139b83e0ed29ef7d13e3754e
alexandr <alexandrov6927@gmail.com>
2019-07-19 00:09:23 +0300

    Updates

---
+++ precache-manifest.c000353fef0e2ca61a1fb823c3d606d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e356c9ccd8d2e57e95b196d618b9b1b9",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "e4c7244b4539f3d0c1d1",
+    "url": "/react_add-todo-form/static/css/main.3714c347.chunk.css"
+  },
+  {
+    "revision": "063f61c702b3922067d9",
+    "url": "/react_add-todo-form/static/js/2.0e531a5a.chunk.js"
+  },
+  {
+    "revision": "e4c7244b4539f3d0c1d1",
+    "url": "/react_add-todo-form/static/js/main.acfbb3a9.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
